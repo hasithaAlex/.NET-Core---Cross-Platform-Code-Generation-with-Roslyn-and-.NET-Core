1e456a387b3cd5fcd126a5e86c64fd3eec5d79fe
hasitha madusanka <hasithamadusanka90_@outlook.com>
2017-11-09 13:42:05 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RoslynCore;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testing");
+        }
+
+    }
+}
