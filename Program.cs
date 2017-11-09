a81e6fe2303ff0ee24cc3b96b511ec95743eb1bc
hasitha madusanka <hasithamadusanka90_@outlook.com>
2017-11-09 16:44:05 +0530

    Create a class with .NET Core and Roslyn and write the output to the console. https://carlos.mendible.com/2017/03/02/create-a-class-with-net-core-and-roslyn/

--- Program.cs
+++ Program.cs
@@ -12,67 +12,17 @@ namespace RoslynCore
     class Program
     {
        public static void Main(string[] args)
-    {
-
-        // intentionally introduce errors to see the compiler reporting code issues
-        const string code = 
-        @"using System; 
-        using System.IO; 
-
-        namespace MathFunctions 
-        { 
-            public static class MathHelper
-            { 
-                public static void CalculateCircleArea() 
-                { 
-                    bouble Radius = 10;
-                    double result = radius * radius * System.Math.PI;
-                    Console.WriteLine(result.ToString()); 
-                } 
-            } 
-        }";
-
-        // Get a SyntaxTree
-        var tree = SyntaxFactory.ParseSyntaxTree(code);
-
-        Console.WriteLine(tree);
-        PrintDiagnostics(tree);
-
-        // Create a compilation for the syntax tree
-        var compilation = CSharpCompilation.Create("mylib.dll").
-            WithOptions(
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)).
-            AddReferences(MetadataReference.CreateFromFile(typeof(object).
-            GetTypeInfo().Assembly.Location)).
-            AddSyntaxTrees(tree);
-
-        var fileName = "mylib.dll";
-        var path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-
-        // Emit an Assembly that contains the result of the Roslyn code generation
-        compilation.Emit(path);
-
-        // Use reflection to load and execute code
-        var asm = AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
-        asm.GetType("MathFunctions.MathHelper").GetMethod("CalculateCircleArea").Invoke(null, new object[]{});
-        Console.ReadLine();
-    }
-
-
-        
-    private static void PrintDiagnostics(SyntaxTree tree)
-    {
-        // detects diagnostics in the source code
-        var diagnostics = tree.GetDiagnostics();
-
-        if (diagnostics.Any())
         {
-            foreach (var diag in diagnostics)
-            {
-                // if any, prints diagnostic message and line/row position
-                Console.WriteLine($"{diag.GetMessage()} {diag.Location.GetLineSpan()}");
-            }
+            /*
+            * Learn how to invoke the C# compiler in .NET Core applications that run on Linux, 
+            * Mac and Windows to perform code generation with the Roslyn APIs, using
+            * Visual Studio Code as the development environment.        
+            */
+            //new DiagnosticSourceCode().runDiagnosticSourceCode();
+
+
+            new GenerateClass().CreateClass();
+
         }
-    }
     }
 }
